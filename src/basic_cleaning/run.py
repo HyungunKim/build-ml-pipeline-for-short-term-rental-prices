51f697353bc6c49cfcbedb175d34d5eea17438d4
Hyunkun Kim <hyungun.kimm@gmail.com>
2025-07-18 05:40:46 +0900

    [step 2: basic cleaning] revert logging customization as it conflicts with hydra logging.

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -6,15 +6,8 @@ import argparse
 import logging
 import wandb
 
-
-logger = logging.getLogger(__name__)
-console_handler = logging.StreamHandler()
-file_handler = logging.FileHandler("basic_cleaning.log")
-file_handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s -%(filename)s/%(funcName)s - %(message)s')
-console_handler.setFormatter(formatter)
-file_handler.setFormatter(formatter)
-logger.setLevel(logging.INFO)
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
 
 def go(args):
 
